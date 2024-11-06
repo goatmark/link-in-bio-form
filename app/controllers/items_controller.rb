4a78649c3930b38d6683e4ef807988033bcd29fb
goatmark <102983476+goatmark@users.noreply.github.com>
2024-11-06 23:43:25 +0000

    1commitdoneworking

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,27 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def backdoor
+
+  
+    render({ :template => "item_templates/backdoor"})
+  end
+
+  def add_link
+
+    @link_description = params.fetch("link_description", "")
+    @link_url = params.fetch("link_url", "")
+    @image_url = params.fetch("image_url", "")
+
+    @i = Item.new
+
+    @i.link_url = @link_url
+    @i.link_description = @link_description
+    @i.thumbnail_url = @image_url
+
+    @i.save
+
+    redirect_to("/")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+
+  get("/backdoor", {:controller => "items", :action => "backdoor"})
+
+  post("/add_link", {:controller=> "items", :action => "add_link"})
 end
